37c6af547a1638d5676b1afcba47ae870cc9df70
天邑 <hongsheng.jhs@alibaba-inc.com>
2024-10-14 09:46:30 +0800

    fix test

--- tzrec/tools/tdm/gen_tree/tree_search_util_test.py
+++ tzrec/tools/tdm/gen_tree/tree_search_util_test.py
@@ -72,7 +72,7 @@ class TreeSearchtest(unittest.TestCase):
             for line in f:
                 serving_tree.append(line)
 
-        self.assertEqual(len(node_table), 14)
+        self.assertEqual(len(node_table), 15)
         self.assertEqual(len(edge_table), 13)
         self.assertEqual(len(predict_edge_table), 13)
         self.assertEqual(len(serving_tree), 14)
