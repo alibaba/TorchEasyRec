2db08f02e0cf957d85a038ccc97f897c43702994
天邑 <hongsheng.jhs@alibaba-inc.com>
2024-10-27 16:15:05 +0800

    upgrade torchrec to 1.0.0

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 REGISTRY=mybigpai-public-registry.cn-beijing.cr.aliyuncs.com/easyrec
-DOCKER_TAG=0.5
+DOCKER_TAG=0.6
 
 cp requirements.txt docker/
 rm -rf docker/requirements
 cp -r requirements/ docker/requirements
 cd docker
 
-docker build -t ${REGISTRY}/tzrec-devel:latest .
-docker images -q ${REGISTRY}/tzrec-devel:latest | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
-docker push ${REGISTRY}/tzrec-devel:latest
+for DEVICE in cu121 cpu
+do
+    docker build --network host -t ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-${DEVICE} --build-arg DEVICE=${DEVICE} .
+    docker push ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-${DEVICE}
+done
+
+docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu121 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
+docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu121 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:latest
 docker push ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
+docker push ${REGISTRY}/tzrec-devel:latest
