fix calculation of weighted loss

tzrec/models/match_model.py
@@ -233,7 +233,9 @@ class MatchModel(BaseModel):
             label = _zero_int_label(pred)
         losses[loss_name] = self._loss_modules[loss_name](pred, label)
         if self._sample_weight:
-            losses[loss_name] = torch.mean(losses[loss_name] * sample_weight)
+            losses[loss_name] = torch.sum(
+                losses[loss_name] * sample_weight
+            ) / torch.sum(sample_weight)
 
         return losses
 

tzrec/models/rank_model.py
@@ -215,7 +215,9 @@ class RankModel(BaseModel):
             raise ValueError(f"loss[{loss_type}] is not supported yet.")
 
         if sample_weight_name:
-            losses[loss_name] = torch.mean(losses[loss_name] * sample_weights)
+            losses[loss_name] = torch.sum(
+                losses[loss_name] * sample_weights
+            ) / torch.sum(sample_weights)
         return losses
 
     def loss(