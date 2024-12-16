[bugfix] fix calculation of weighted loss (#65)

tzrec/models/match_model.py
@@ -217,7 +217,9 @@ class MatchModel(BaseModel):
         losses = {}
         label = batch.labels[label_name]
         sample_weight = (
-            batch.sample_weights[self._sample_weight] if self._sample_weight else 1.0
+            batch.sample_weights[self._sample_weight]
+            if self._sample_weight
+            else torch.Tensor([1.0])
         )
 
         loss_type = loss_cfg.WhichOneof("loss")
@@ -233,7 +235,9 @@ class MatchModel(BaseModel):
             label = _zero_int_label(pred)
         losses[loss_name] = self._loss_modules[loss_name](pred, label)
         if self._sample_weight:
-            losses[loss_name] = torch.mean(losses[loss_name] * sample_weight)
+            losses[loss_name] = torch.mean(
+                losses[loss_name] * sample_weight
+            ) / torch.mean(sample_weight)
 
         return losses
 

tzrec/models/rank_model.py
@@ -189,7 +189,9 @@ class RankModel(BaseModel):
         losses = {}
         label = batch.labels[label_name]
         sample_weights = (
-            batch.sample_weights[sample_weight_name] if sample_weight_name else None
+            batch.sample_weights[sample_weight_name]
+            if sample_weight_name
+            else torch.Tensor([1.0])
         )
 
         loss_type = loss_cfg.WhichOneof("loss")
@@ -215,7 +217,9 @@ class RankModel(BaseModel):
             raise ValueError(f"loss[{loss_type}] is not supported yet.")
 
         if sample_weight_name:
-            losses[loss_name] = torch.mean(losses[loss_name] * sample_weights)
+            losses[loss_name] = torch.mean(
+                losses[loss_name] * sample_weights
+            ) / torch.mean(sample_weights)
         return losses
 
     def loss(