6b9f6e77f8500185507c54912f48db5b6867aa62
天邑 <hongsheng.jhs@alibaba-inc.com>
2025-02-05 10:20:53 +0800

    remove forward impl

--- tzrec/acc/quant_utils.py
+++ tzrec/acc/quant_utils.py
@@ -15,12 +15,6 @@ from typing import Any, Dict, List, Optional, Tuple, Type, cast
 import torch
 import torch.nn as nn
 import torch.quantization as quant
-from fbgemm_gpu.split_table_batched_embeddings_ops_common import (
-    BoundsCheckMode,
-)
-from fbgemm_gpu.split_table_batched_embeddings_ops_inference import (
-    IntNBitTableBatchedEmbeddingBagsCodegen,
-)
 from torch import Tensor
 from torchrec.modules.embedding_configs import (
     BaseEmbeddingConfig,
@@ -46,115 +40,6 @@ from torchrec.quant.embedding_modules import (
     EmbeddingBagCollection as QuantEmbeddingBagCollection,
 )
 from torchrec.sparse.jagged_tensor import KeyedJaggedTensor, KeyedTensor
-
-
-# we remove inputs_to_device temporally to make IntNBitTableBatchedEmbeddingBagsCodegen
-# support torch.jit.script
-def _forward_impl(
-    self,
-    indices: Tensor,
-    offsets: Tensor,
-    per_sample_weights: Optional[Tensor] = None,
-) -> Tensor:
-    assert self.weight_initialized, (
-        "weight needs to be initialized before forward function"
-    )
-
-    # indices, offsets, per_sample_weights = inputs_to_device(
-    #     indices, offsets, per_sample_weights, self.bounds_check_warning.device
-    # )
-
-    # First bound check: check if the indices/offsets are within the boundary
-    # of the original embedding rows before pruning.
-    # Note that this is only applied when we enable pruning (if the perf becomes
-    # an issue, we can fuse it inside the remapping kernel).
-    if (
-        self.index_remapping_hash_table_cpu is not None
-        or self.index_remapping_hash_table.numel() > 0
-        or self.index_remappings_array.numel() > 0
-    ):
-        if self.bounds_check_mode_int != BoundsCheckMode.NONE.value:
-            torch.ops.fbgemm.bounds_check_indices(
-                self.original_rows_per_table,
-                indices,
-                offsets,
-                self.bounds_check_mode_int,
-                self.bounds_check_warning,
-                per_sample_weights,
-            )
-
-    # Index remapping changes input indices, and some of them becomes -1 (prunned rows).
-    # Hence, remapping should be done before prefetch and emb lookup
-    # so that these operations are with the remapped indices.
-    if self.index_remapping_hash_table_cpu is not None:
-        indices = self.index_remapping_hash_table_cpu.lookup(indices, offsets)
-    elif self.index_remapping_hash_table.numel() > 0:
-        # Convert from raw indices to pruned indices
-        indices = torch.ops.fbgemm.pruned_hashmap_lookup(
-            indices,
-            offsets,
-            self.index_remapping_hash_table,
-            self.index_remapping_hash_table_offsets,
-        )
-    elif self.index_remappings_array.numel() > 0:
-        indices = torch.ops.fbgemm.pruned_array_lookup(
-            indices,
-            offsets,
-            self.index_remappings_array,
-            self.index_remappings_array_offsets,
-        )
-    # pyre-fixme[29]: `Union[(self: TensorBase) -> int, Module, Tensor]` is not
-    #  a function.
-    if self.lxu_cache_weights.numel() > 0:
-        if self.timestep_prefetch_size.get() <= 0:
-            self.prefetch(indices, offsets)
-        self.timestep_prefetch_size.decrement()
-
-    lxu_cache_locations = self.lxu_cache_locations_list.pop()
-
-    # Second bound check: check if the indices/offsets are within the boundary
-    # of the pruned embedding rows after pruning.
-    # Note: we cast to int as a TorchScript workaround.
-    if self.bounds_check_mode_int != BoundsCheckMode.NONE.value:
-        torch.ops.fbgemm.bounds_check_indices(
-            self.rows_per_table,
-            indices,
-            offsets,
-            self.bounds_check_mode_int,
-            self.bounds_check_warning,
-            per_sample_weights,
-        )
-    # Note: CPU and CUDA ops use the same interface to facilitate JIT IR
-    # generation for CUDA/CPU. For CPU op, we don't need weights_uvm and
-    # weights_placements
-    return torch.ops.fbgemm.int_nbit_split_embedding_codegen_lookup_function(
-        dev_weights=self.weights_host if self.host_size > 0 else self.weights_dev,
-        uvm_weights=self.weights_uvm,
-        weights_placements=self.weights_placements,
-        weights_offsets=self.weights_offsets,
-        weights_tys=self.weights_tys,
-        D_offsets=self.D_offsets,
-        total_D=self.total_D,
-        max_int2_D=self.max_int2_D,
-        max_int4_D=self.max_int4_D,
-        max_int8_D=self.max_int8_D,
-        max_float16_D=self.max_float16_D,
-        max_float32_D=self.max_float32_D,
-        indices=indices,
-        offsets=offsets,
-        pooling_mode=int(self.pooling_mode),
-        indice_weights=per_sample_weights,
-        output_dtype=self.output_dtype,
-        lxu_cache_weights=self.lxu_cache_weights,
-        lxu_cache_locations=lxu_cache_locations,
-        row_alignment=self.row_alignment,
-        max_float8_D=self.max_float8_D,
-        fp8_exponent_bits=self.fp8_exponent_bits,
-        fp8_exponent_bias=self.fp8_exponent_bias,
-    )
-
-
-IntNBitTableBatchedEmbeddingBagsCodegen._forward_impl = _forward_impl
 
 
 class QuantManagedCollisionEmbeddingBagCollection(QuantEmbeddingBagCollection):
@@ -211,9 +96,9 @@ class QuantManagedCollisionEmbeddingBagCollection(QuantEmbeddingBagCollection):
             quant_state_dict_split_scale_bias,
             row_alignment,
         )
-        assert managed_collision_collection, (
-            "Managed collision collection cannot be None"
-        )
+        assert (
+            managed_collision_collection
+        ), "Managed collision collection cannot be None"
         self._managed_collision_collection: ManagedCollisionCollection = (
             managed_collision_collection
         )
