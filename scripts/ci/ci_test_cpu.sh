9492ffc2da255324a1ecc64c9855b3a1ffeb185f
Hongsheng Jin <hongsheng.jhs@alibaba-inc.com>
2025-04-27 17:33:21 +0800

    [feat] add faiss gpu support (#170)
    speed up hitrate calculate 8x

--- scripts/ci/ci_test_cpu.sh
+++ scripts/ci/ci_test_cpu.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-pip install -r requirements/runtime.txt
-pip install -r requirements/test.txt
+pip install -r requirements-cpu.txt
 bash scripts/gen_proto.sh
 
 MKL_THREADING_LAYER=GNU PYTHONPATH=. python tzrec/tests/run.py
