e2fe3907f449d9b160fa4069ce364b44626a801f
gecheng <eric.gc@alibaba-inc.com>
2025-08-14 10:28:37 +0800

    sampling xauc and in-batch xauc

--- tzrec/metrics/xauc.py
+++ tzrec/metrics/xauc.py
@@ -29,28 +29,31 @@ class XAUC(Metric):
             Actual number of sample pairs is n*(n-1)/2 * ratio. Reduce the
             ratio when eval set is huge and GPU memory is limited. Default
             value 1.0.
-        memory_saving(bool): Use memory saving mode or not. When True,
+        in_batch(bool): Get sample pair within a batch. When True,
             sampling is done per batch, xauc is calculated batch-wise and
             finally averaged, sample_ratio is ignored. Otherwise, xauc is
-            calculated on the whole eval set. Default value False.
+            calculated on the whole eval set. Pls note that in_batch
+            is typically memory efficient and faster, but may have bias.
+            Better to shuffle your eval dataset before doing in_batch xauc.
+            Default value False.
 
     """
 
     def __init__(
-        self, sample_ratio: float = 1.0, memory_saving: bool = False, **kwargs: Any
+        self, sample_ratio: float = 1.0, in_batch: bool = False, **kwargs: Any
     ) -> None:
         super().__init__(**kwargs)
         assert sample_ratio > 0 and sample_ratio <= 1.0, (
             "sample_ratio must be between (0, 1]"
         )
-
         self.sample_ratio = sample_ratio
-        self.memory_saving = memory_saving
+        self.in_batch = in_batch
 
-        if memory_saving:
+        if in_batch:
             self.add_state("batch_xauc", default=[], dist_reduce_fx="cat")
         else:
-            self.add_state("eval_data", default=[], dist_reduce_fx="cat")
+            self.add_state("eval_preds", default=[], dist_reduce_fx="cat")
+            self.add_state("eval_targets", default=[], dist_reduce_fx="cat")
             self.add_state(
                 "total_sample_count", default=torch.tensor(0), dist_reduce_fx="sum"
             )
@@ -62,59 +65,87 @@ class XAUC(Metric):
             preds (Tensor): a float 1d-tensor of predictions.
             targets (Tensor): a integer 1d-tensor of target.
         """
-        if self.memory_saving:
-            n = targets.shape[0]
-            batch_xauc = self.sampling_auc(preds, targets, n, n)
+        if self.in_batch:
+            batch_xauc = self.in_batch_xauc(preds, targets)
             self.batch_xauc.append(batch_xauc)
         else:
-            self.eval_data.append({"preds": preds, "targets": targets})
-            self.total_sample_count += preds.shape[0]
+            self.eval_preds.append(preds)
+            self.eval_targets.append(targets)
+            self.total_sample_count += torch.tensor(preds.shape[0])
+
+        self.batch_size = int(preds.shape[0])
 
     def compute(self) -> torch.Tensor:
         """Compute the metric."""
-        if self.memory_saving:
+        if self.in_batch:
             return self.batch_xauc.mean()
         else:
-            n = self.total_sample_count
-            max_pairs = self.total_sample_count * (self.total_sample_count - 1) // 2
-            n_sample_pairs = int(max_pairs * self.sample_ratio)
+            preds = self.eval_preds
+            target = self.eval_targets
 
-            xauc = self.sampling_auc(
-                self.eval_data["preds"], self.eval_data["targets"], n, n_sample_pairs
-            )
+            n = self.total_sample_count
+            n_sample_pairs = int(n * (n - 1) // 2 * self.sample_ratio)
+
+            xauc = self.sampling_xauc(preds, target, n, n_sample_pairs, self.batch_size)
             return xauc
 
-            # all_pairs = torch.triu_indices(n, n, offset=1).t()
-            # sampled_indices = torch.randperm(max_pairs)[:n_sample_pairs]
-            # sampled_pairs = all_pairs[sampled_indices]
-
-            # idx_i = sampled_pairs[:, 0]
-            # idx_j = sampled_pairs[:, 1]
-
-            # preds_i = self.eval_data['preds'][idx_i]
-            # preds_j = self.eval_data['preds'][idx_j]
-            # targets_i = self.eval_data['targets'][idx_i]
-            # targets_j = self.eval_data['targets'][idx_j]
-
-            # correct = ((targets_i - targets_j) * (preds_i - preds_j) > 0).float()
-            # xauc = correct.mean()
-            # return xauc
-
-    def sampling_auc(
-        self, preds: torch.Tensor, targets: torch.Tensor, n: int, n_sample_pairs: int
-    ):
+    def sampling_xauc(
+        self,
+        preds: torch.Tensor,
+        targets: torch.Tensor,
+        n: int,
+        n_sample_pairs: int,
+        batch_size: int,
+    ) -> torch.Tensor:
         """Sample pairs."""
-        max_pairs = n * (n - 1) // 2
-        all_pairs = torch.triu_indices(n, n, offset=1).t()
-        sampled_indices = torch.randperm(max_pairs)[:n_sample_pairs]
-        sampled_pairs = all_pairs[sampled_indices]
-        idx_i = sampled_pairs[:, 0]
-        idx_j = sampled_pairs[:, 1]
+        n = int(n)
+        total_pairs = n * (n - 1) // 2
+        n_sample_pairs = int(n_sample_pairs)
+
+        idx_i = []
+        idx_j = []
+
+        n_batch = n_sample_pairs // batch_size
+
+        for _ in range(n_batch):
+            sampled_flat = torch.randint(0, total_pairs, (batch_size,))
+
+            i = torch.floor((-1 + torch.sqrt(1 + 8 * sampled_flat.float())) / 2).long()
+            base = i * (i + 1) // 2
+            batch_idx_i = sampled_flat - base
+            batch_idx_j = i + 1
+
+            idx_i.append(batch_idx_i)
+            idx_j.append(batch_idx_j)
+
+        idx_i = torch.concat(idx_i)
+        idx_j = torch.concat(idx_j)
+
+        idx_i = idx_i.to(preds.device)
+        idx_j = idx_j.to(preds.device)
 
         preds_i = preds[idx_i]
         preds_j = preds[idx_j]
         targets_i = targets[idx_i]
         targets_j = targets[idx_j]
+
+        correct = ((targets_i - targets_j) * (preds_i - preds_j) > 0).float()
+        xauc = correct.mean()
+        return xauc
+
+    def in_batch_xauc(
+        self, batch_preds: torch.Tensor, batch_targets: torch.Tensor
+    ) -> torch.Tensor:
+        """Xauc in a batch."""
+        b = batch_preds.shape[0]
+        all_pairs = torch.triu_indices(b, b, offset=1).t()
+        idx_i = all_pairs[:, 0]
+        idx_j = all_pairs[:, 1]
+
+        preds_i = batch_preds[idx_i]
+        preds_j = batch_preds[idx_j]
+        targets_i = batch_targets[idx_i]
+        targets_j = batch_targets[idx_j]
 
         correct = ((targets_i - targets_j) * (preds_i - preds_j) > 0).float()
         xauc = correct.mean()
