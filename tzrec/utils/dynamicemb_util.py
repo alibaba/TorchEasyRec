6958da38d5ea4c602793353223204de3c1d70d97
天邑 <hongsheng.jhs@alibaba-inc.com>
2025-09-25 18:03:33 +0800

    add init_capacity_per_rank

---
+++ tzrec/utils/dynamicemb_util.py
@@ -0,0 +1,279 @@
+# Copyright (c) 2024, Alibaba Group;
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+from functools import partial  # NOQA
+from typing import List, Optional, cast
+
+from torch import nn  # NOQA
+from torchrec.distributed.embedding_types import EmbeddingComputeKernel
+from torchrec.distributed.planner import planners
+from torchrec.distributed.planner.types import (
+    ParameterConstraints,
+    ShardingOption,
+    Topology,
+)
+from torchrec.distributed.sharding_plan import placement
+from torchrec.distributed.types import (
+    CacheParams,
+    EmbeddingModuleShardingPlan,
+    EnumerableShardingSpec,
+    ParameterSharding,
+    ShardingPlan,
+    ShardingType,
+    ShardMetadata,
+)
+from torchrec.modules.embedding_configs import BaseEmbeddingConfig
+from torchrec.modules.mc_modules import (
+    average_threshold_filter,  # NOQA
+    dynamic_threshold_filter,  # NOQA
+    probabilistic_threshold_filter,  # NOQA
+)
+
+from tzrec.protos import feature_pb2
+
+has_dynamicemb = False
+try:
+    import dynamicemb
+    from dynamicemb import (
+        DynamicEmbEvictStrategy,
+        DynamicEmbInitializerArgs,
+        DynamicEmbInitializerMode,
+        DynamicEmbScoreStrategy,
+    )
+    from dynamicemb.dynamicemb_config import DynamicEmbKernel
+    from dynamicemb.planner import (
+        DynamicEmbParameterConstraints,
+        DynamicEmbParameterSharding,
+    )
+
+    has_dynamicemb = True
+except Exception:
+    pass
+
+
+def _next_power_of_2(n):
+    # Handle the case where n is 0
+    if n == 0:
+        return 1
+
+    # If n is already a power of 2, return n
+    if (n & (n - 1)) == 0:
+        return n
+
+    # Find the next power of 2
+    n -= 1
+    n |= n >> 1
+    n |= n >> 2
+    n |= n >> 4
+    n |= n >> 8
+    n |= n >> 16
+    n |= n >> 32  # This line is necessary for 64-bit integers
+    return n + 1
+
+
+def _build_dynamicemb_initializer(
+    init_cfg: Optional[feature_pb2.DynamicEmbInitializerArgs],
+    num_embeddings: int,
+    embedding_dim: int,
+    is_eval: bool = False,
+) -> "DynamicEmbInitializerArgs":
+    """Build initializer for Dynamic Embedding."""
+    if init_cfg is None:
+        if is_eval:
+            return DynamicEmbInitializerArgs(mode=DynamicEmbInitializerMode.CONSTANT)
+        else:
+            # compatible with torchrec init
+            return DynamicEmbInitializerArgs(
+                mode=DynamicEmbInitializerMode.UNIFORM,
+                lower=-1 / math.sqrt(num_embeddings),
+                upper=1 / math.sqrt(num_embeddings),
+            )
+    init_kwargs = {}
+    if init_cfg.HasField("mode"):
+        mode = init_cfg.mode
+    elif is_eval:
+        mode = "CONSTANT"
+    else:
+        mode = "UNIFORM"
+    if mode in DynamicEmbInitializerMode.__members__.keys():
+        init_kwargs["mode"] = DynamicEmbInitializerMode[mode]
+    else:
+        raise ValueError(
+            "DynamicEmb init mode support: "
+            f"{DynamicEmbInitializerMode.__members__.keys}, but got {mode}."
+        )
+
+    if mode in ["NORMAL", "TRUNCATED_NORMAL"]:
+        init_kwargs["mean"] = init_cfg.mean
+        init_kwargs["std_dev"] = (
+            init_cfg.std_dev
+            if init_cfg.HasField("std_dev")
+            else 1 / math.sqrt(embedding_dim)
+        )
+    elif mode == "UNIFORM":
+        # compatible with torchrec init
+        init_kwargs["lower"] = (
+            init_cfg.lower
+            if init_cfg.HasField("lower")
+            else -1 / math.sqrt(num_embeddings)
+        )
+        init_kwargs["upper"] = (
+            init_cfg.upper
+            if init_cfg.HasField("upper")
+            else 1 / math.sqrt(num_embeddings)
+        )
+    elif mode == "CONSTANT":
+        init_kwargs["value"] = init_cfg.value
+    return DynamicEmbInitializerArgs(**init_kwargs)
+
+
+def build_dynamicemb_constraints(
+    dynamicemb_cfg: feature_pb2.DynamicEmbedding, emb_config: BaseEmbeddingConfig
+) -> ParameterConstraints:
+    """Build ParameterConstraints for DynamicEmbedding."""
+    embedding_dim = emb_config.embedding_dim
+    num_embeddings = emb_config.num_embeddings
+
+    evict_strategy = None
+    if dynamicemb_cfg.evict_strategy in DynamicEmbEvictStrategy.__members__.keys():
+        evict_strategy = DynamicEmbEvictStrategy[dynamicemb_cfg.evict_strategy]
+    else:
+        raise ValueError(
+            "DynamicEmbEvictStrategy support: "
+            f"{DynamicEmbEvictStrategy.__members__.keys()}, "
+            "but got {dynamicemb_cfg.evict_strategy}."
+        )
+    score_strategy = None
+    if dynamicemb_cfg.score_strategy in DynamicEmbScoreStrategy.__members__.keys():
+        score_strategy = DynamicEmbScoreStrategy[dynamicemb_cfg.score_strategy]
+    else:
+        raise ValueError(
+            f"DynamicEmbScoreStrategy support: "
+            f"{DynamicEmbScoreStrategy.__members__.keys()}, "
+            f"but got {dynamicemb_cfg.score_strategy}."
+        )
+
+    if dynamicemb_cfg.HasField("init_capacity_per_rank"):
+        dynamicemb_cfg.init_capacity = _next_power_of_2(
+            dynamicemb_cfg.init_capacity_per_rank
+        )
+
+    dynamicemb_options = dynamicemb.DynamicEmbTableOptions(
+        max_capacity=dynamicemb_cfg.max_capacity,
+        # TODO: convert eb_config.init_fn to dynamicemb initializer
+        initializer_args=_build_dynamicemb_initializer(
+            dynamicemb_cfg.initializer_args, num_embeddings, embedding_dim
+        ),
+        eval_initializer_args=_build_dynamicemb_initializer(
+            dynamicemb_cfg.eval_initializer_args,
+            num_embeddings,
+            embedding_dim,
+            is_eval=True,
+        ),
+        evict_strategy=evict_strategy,
+        score_strategy=score_strategy,
+    )
+
+    constraints = DynamicEmbParameterConstraints(
+        use_dynamicemb=True,
+        sharding_types=[ShardingType.ROW_WISE.value],
+        compute_kernels=[
+            EmbeddingComputeKernel.DRAM_VIRTUAL_TABLE.value
+        ],  # workaround for ShardingPlan estimator
+        cache_params=CacheParams(
+            load_factor=dynamicemb_cfg.cache_load_factor
+        ),  # for ShardingPlan estimator hdm storage
+        dynamicemb_options=dynamicemb_options,
+    )
+    return constraints
+
+
+if has_dynamicemb:
+
+    def _to_sharding_plan(
+        sharding_options: List[ShardingOption],
+        topology: Topology,
+    ) -> ShardingPlan:
+        compute_device = topology.compute_device
+        local_size = topology.local_world_size
+        world_size = topology.world_size
+
+        plan = {}
+        for sharding_option in sharding_options:
+            shards = sharding_option.shards
+            sharding_type = sharding_option.sharding_type
+
+            module_plan = plan.get(sharding_option.path, EmbeddingModuleShardingPlan())
+
+            sharding_spec = (
+                None
+                if sharding_type == ShardingType.DATA_PARALLEL.value
+                else EnumerableShardingSpec(
+                    [
+                        ShardMetadata(
+                            shard_sizes=shard.size,
+                            shard_offsets=shard.offset,
+                            placement=placement(
+                                compute_device, cast(int, shard.rank), local_size
+                            ),
+                        )
+                        for shard in shards
+                    ]
+                )
+            )
+
+            if (
+                hasattr(sharding_option, "use_dynamicemb")
+                and sharding_option.use_dynamicemb
+            ):
+                # only support row-wise now
+                dynamicemb_options = sharding_option.dynamicemb_options
+
+                # align to next_power_of_2
+                dynamicemb_options.local_hbm_for_values = sharding_option.shards[
+                    0
+                ].storage.hbm
+                num_embeddings_per_shard = shards[0].size[0]
+                num_aligned_embedding_per_rank = _next_power_of_2(shards[0].size[0])
+                if num_aligned_embedding_per_rank < dynamicemb_options.bucket_capacity:
+                    num_aligned_embedding_per_rank = dynamicemb_options.bucket_capacity
+                if num_embeddings_per_shard != num_aligned_embedding_per_rank:
+                    dynamicemb_options.num_aligned_embedding_per_rank = (
+                        num_aligned_embedding_per_rank
+                    )
+
+                module_plan[sharding_option.name] = DynamicEmbParameterSharding(
+                    sharding_spec=sharding_spec,
+                    sharding_type=ShardingType.ROW_WISE.value,
+                    ranks=[i for i in range(world_size)],
+                    compute_kernel=EmbeddingComputeKernel.CUSTOMIZED_KERNEL.value,
+                    customized_compute_kernel=DynamicEmbKernel,
+                    dist_type="roundrobin",
+                    dynamicemb_options=dynamicemb_options,
+                )
+            else:
+                module_plan[sharding_option.name] = ParameterSharding(
+                    sharding_spec=sharding_spec,
+                    sharding_type=sharding_type,
+                    compute_kernel=sharding_option.compute_kernel,
+                    ranks=[cast(int, shard.rank) for shard in shards],
+                    cache_params=sharding_option.cache_params,
+                    enforce_hbm=sharding_option.enforce_hbm,
+                    stochastic_rounding=sharding_option.stochastic_rounding,
+                    bounds_check_mode=sharding_option.bounds_check_mode,
+                    output_dtype=sharding_option.output_dtype,
+                    key_value_params=sharding_option.key_value_params,
+                )
+                plan[sharding_option.path] = module_plan
+        return ShardingPlan(plan)
+
+    planners.to_sharding_plan = _to_sharding_plan
