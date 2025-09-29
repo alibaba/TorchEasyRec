70840186f286cb1d542a9b0b0851096f0c354197
天邑 <hongsheng.jhs@alibaba-inc.com>
2025-09-29 11:26:09 +0800

    revert build

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -8,13 +8,13 @@ cp -r requirements*.txt docker/
 cp -r requirements/ docker/requirements
 cd docker
 
-for DEVICE in cu126
+for DEVICE in cpu cu126
 do
-    docker build --network host -t ${REGISTRY}/tzrec-test:${DOCKER_TAG}-${DEVICE}-rdma3 --build-arg DEVICE=${DEVICE} .
-    docker push ${REGISTRY}/tzrec-test:${DOCKER_TAG}-${DEVICE}-rdma3
+    docker build --network host -t ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-${DEVICE} --build-arg DEVICE=${DEVICE} .
+    docker push ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-${DEVICE}
 done
 
-# docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu126 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
-# docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu126 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:latest
-# docker push ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
-# docker push ${REGISTRY}/tzrec-devel:latest
+docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu126 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
+docker images -q ${REGISTRY}/tzrec-devel:${DOCKER_TAG}-cu126 | xargs -I {} docker tag {} ${REGISTRY}/tzrec-devel:latest
+docker push ${REGISTRY}/tzrec-devel:${DOCKER_TAG}
+docker push ${REGISTRY}/tzrec-devel:latest
