a8fe90e557a55debee26df3b3193b968a9747752
chengaofei <wb-cgf552770@alibaba-inc.com>
2025-10-21 03:48:17 +0000

    fix test case error

--- tzrec/features/sequence_feature.py
+++ tzrec/features/sequence_feature.py
@@ -519,8 +519,6 @@ class SequenceRawFeature(RawFeature):
             fg_cfg["boundaries"] = list(self.config.boundaries)
         if self.config.HasField("value_dim"):
             fg_cfg["value_dim"] = self.config.value_dim
-        else:
-            fg_cfg["value_dim"] = 1
         if self.config.HasField("stub_type"):
             fg_cfg["stub_type"] = self.config.stub_type
         return [fg_cfg]
