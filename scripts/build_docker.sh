41c1a82d343a12cd8b1f736cbba13f4292265a43
天邑 <hongsheng.jhs@alibaba-inc.com>
2026-02-27 10:47:27 +0800

    bump up version

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -10,13 +10,13 @@ cp -r requirements*.txt docker/
 cp -r requirements/ docker/requirements
 cd docker
 
-for DEVICE in cpu cu126
+for DEVICE in cu126
 do
     docker build --network host -t ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX} --build-arg DEVICE=${DEVICE} .
     docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX}
 done
 
-docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu126${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
-docker images -q ${REGISTRY}${REPO_NAME}:${DOCKER_TAG}-cu126${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
+docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
+docker images -q ${REGISTRY}${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
 docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
 docker push ${REGISTRY}/${REPO_NAME}:latest
