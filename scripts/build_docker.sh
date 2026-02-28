6e39c38f6c125d3f33b4af60774642bb6d613446
天邑 <hongsheng.jhs@alibaba-inc.com>
2026-02-28 17:50:40 +0800

    docker add py310 py312

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 REGISTRY=mybigpai-public-registry.cn-beijing.cr.aliyuncs.com/easyrec
 REPO_NAME=tzrec-test
@@ -10,13 +11,19 @@ cp -r requirements*.txt docker/
 cp -r requirements/ docker/requirements
 cd docker
 
-for DEVICE in cu126
+for DEVICE in cpu cu126 cu129
 do
-    docker build --network host -t ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX} --build-arg DEVICE=${DEVICE} .
+    for PYTHON_VERSION in 3.10 3.11 3.12
+    do
+        PY_VERSION=py${PYTHON_VERSION//./}
+        docker build --network host -t ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-${PY_VERSION}${DOCKER_TAG_SUFFIX} --build-arg DEVICE=${DEVICE} --build-arg PYTHON_VERSION=${PYTHON_VERSION} .
+        docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-${PY_VERSION}${DOCKER_TAG_SUFFIX}
+    done
+    docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-py311${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX}
     docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX}
 done
 
-docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
-docker images -q ${REGISTRY}${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
+docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu129-${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
+docker images -q ${REGISTRY}${REPO_NAME}:${DOCKER_TAG}-cu129-${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
 docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
 docker push ${REGISTRY}/${REPO_NAME}:latest
