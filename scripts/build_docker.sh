a1fe55ce8b56ccd82bd45e467cac29e89e7dbcec
天邑 <hongsheng.jhs@alibaba-inc.com>
2026-03-19 14:52:36 +0000

    use new fbgemm wheel

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
+#!/usr/bin/env bash
 
 REGISTRY=mybigpai-public-registry.cn-beijing.cr.aliyuncs.com/easyrec
-REPO_NAME=tzrec-devel
+REPO_NAME=tzrec-test
 DOCKER_TAG=1.1
-DOCKER_TAG_SUFFIX=
+DOCKER_TAG_SUFFIX=-u1
 
 rm -rf docker/requirements*
 cp -r requirements*.txt docker/
@@ -13,17 +14,11 @@ cd docker
 
 for DEVICE in cpu cu126 cu129
 do
-    for PYTHON_VERSION in 3.10 3.11 3.12
-    do
-        PY_VERSION=py${PYTHON_VERSION//./}
-        docker build --network host -t ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-${PY_VERSION}${DOCKER_TAG_SUFFIX} --build-arg DEVICE=${DEVICE} --build-arg PYTHON_VERSION=${PYTHON_VERSION} .
-        docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-${PY_VERSION}${DOCKER_TAG_SUFFIX}
-    done
-    docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}-py311${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX}
+    docker build --network host -t ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX} --build-arg DEVICE=${DEVICE} .
     docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-${DEVICE}${DOCKER_TAG_SUFFIX}
 done
 
 docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
-docker images -q ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
+docker images -q ${REGISTRY}${REPO_NAME}:${DOCKER_TAG}-cu129${DOCKER_TAG_SUFFIX} | xargs -I {} docker tag {} ${REGISTRY}/${REPO_NAME}:latest
 docker push ${REGISTRY}/${REPO_NAME}:${DOCKER_TAG}${DOCKER_TAG_SUFFIX}
 docker push ${REGISTRY}/${REPO_NAME}:latest
