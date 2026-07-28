b96d868aed3c1a15552c206bfc9a35aafe2a8f0c
天邑 <hongsheng.jhs@alibaba-inc.com>
2026-07-28 11:42:22 +0800

    [test] remove optimizer EMA coverage

--- tzrec/optim/optimizer_test.py
+++ tzrec/optim/optimizer_test.py
@@ -12,10 +12,8 @@
 import unittest
 
 import torch
-from torch.amp import GradScaler
 from torchrec.optim import KeyedOptimizerWrapper
 
-from tzrec.optim.ema import DenseEMA, EMAOptimizer
 from tzrec.optim.optimizer import TZRecOptimizer
 
 
@@ -54,48 +52,6 @@ class TZRecOptimizerTest(unittest.TestCase):
         torch.testing.assert_close(param_1, torch.tensor([0.9980, 1.9960]))
         optimizer.zero_grad()
         torch.testing.assert_close(param_1.grad, None)
-
-    def test_ema_updates_only_on_gradient_accumulation_boundary(self):
-        param_1 = torch.tensor([1.0], requires_grad=True)
-        dense_ema = DenseEMA({"param_1": param_1}, decay=0.5)
-        keyed_optimizer = KeyedOptimizerWrapper(
-            {"param_1": param_1}, lambda params: torch.optim.SGD(params, lr=1.0)
-        )
-        optimizer = TZRecOptimizer(
-            EMAOptimizer(keyed_optimizer, dense_ema),
-            gradient_accumulation_steps=2,
-        )
-
-        param_1.grad = torch.tensor([-1.0])
-        optimizer.step()
-        self.assertEqual(dense_ema.n_averaged.item(), 0)
-        torch.testing.assert_close(param_1, torch.tensor([1.0]))
-
-        optimizer.step()
-        self.assertEqual(dense_ema.n_averaged.item(), 1)
-        torch.testing.assert_close(param_1, torch.tensor([2.0]))
-        torch.testing.assert_close(
-            dense_ema.named_averaged_parameters()["param_1"],
-            torch.tensor([2.0]),
-        )
-
-    def test_ema_does_not_update_when_grad_scaler_skips_step(self):
-        param_1 = torch.tensor([1.0], requires_grad=True)
-        dense_ema = DenseEMA({"param_1": param_1}, decay=0.5)
-        keyed_optimizer = KeyedOptimizerWrapper(
-            {"param_1": param_1}, lambda params: torch.optim.SGD(params, lr=1.0)
-        )
-        grad_scaler = GradScaler("cpu")
-        optimizer = TZRecOptimizer(
-            EMAOptimizer(keyed_optimizer, dense_ema),
-            grad_scaler=grad_scaler,
-        )
-
-        grad_scaler.scale(param_1.sum() * torch.tensor(float("inf"))).backward()
-        optimizer.step()
-
-        self.assertEqual(dense_ema.n_averaged.item(), 0)
-        torch.testing.assert_close(param_1, torch.tensor([1.0]))
 
 
 if __name__ == "__main__":
